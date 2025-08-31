f1cb4eb802a786bef29efbe0d16ac3032d96d08a
totaflix <starastar230@gmail.com>
2025-08-31 17:31:42 +0600

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,6 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url=https://t.me/codecrafters_chat/54327') # How to open tutorial link for verification
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
